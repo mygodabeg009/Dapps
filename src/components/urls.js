e3c47253bf2c582324836e7e76b4a6f3fd99839a
mygodabeg009 <mygodabeg009@gmail.com>
2026-08-15 14:11:51 +0100

    Update urls.js

--- src/components/urls.js
+++ src/components/urls.js
@@ -1,3 +1,3 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5050";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://dapp-api.vercel.app";
 
 export default BASE_URL;
